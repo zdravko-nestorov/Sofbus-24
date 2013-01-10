26300d42208eca2e5bc07758f40984ea01fb16a6
zanio89@gmail.com <zanio89@gmail.com@32671852-0f69-9b20-fc9e-79ad69c838e8>
2013-01-10 15:56:07 +0000

    Creating method for time extraction.

--- Sofbus24/src/bg/znestorov/sofbus24/utils/Utils.java
+++ Sofbus24/src/bg/znestorov/sofbus24/utils/Utils.java
@@ -1,5 +1,7 @@
 package bg.znestorov.sofbus24.utils;
 
+import java.math.BigDecimal;
+
 // Creating methods for easy processing data
 public class Utils {
 
@@ -21,4 +23,46 @@ public class Utils {
 		}
 	}
 
+	// Get the difference between two hours in format HH:MM
+	public static String getDiff(String afterTime, String currTime) {
+		String diff = "";
+		int afterTimeMilis = 0;
+		int currTimeMilis = 0;
+
+		try {
+			afterTimeMilis = new BigDecimal(afterTime.split(":")[0]).intValue()
+					* 60 + new BigDecimal(afterTime.split(":")[1]).intValue();
+			currTimeMilis = new BigDecimal(currTime.split(":")[0]).intValue()
+					* 60 + new BigDecimal(currTime.split(":")[1]).intValue();
+
+			diff = (afterTimeMilis - currTimeMilis) + "";
+			diff = (new BigDecimal(diff).intValue() / 60) + ":"
+					+ (new BigDecimal(diff).intValue() % 60);
+			diff = formatTime(diff);
+		} catch (Exception e) {
+
+		}
+
+		diff = afterTimeMilis + " - " + currTimeMilis + " = " + diff;
+
+		return diff;
+	}
+
+	// Format Date (making the minutes in format :XX)
+	public static String formatTime(String difference) {
+		String diff = "";
+		String[] differenceArr = difference.split(":");
+
+		if (differenceArr.length == 2) {
+			if (differenceArr[1].length() == 0) {
+				differenceArr[1] = "00";
+			} else if (differenceArr[1].length() == 1) {
+				differenceArr[1] = "0" + differenceArr[1];
+			}
+
+			diff = differenceArr[0] + ":" + differenceArr[1];
+		}
+
+		return diff;
+	}
 }
