439a39ac2818ef0973cb0fd5dfcf96320184b132
zanio89 <zanio89@32671852-0f69-9b20-fc9e-79ad69c838e8>
2014-03-27 14:32:19 +0000

    Changing METRO URL addresses...

--- HoloDesign-ver.3/Sofbus24/src/bg/znestorov/sofbus24/utils/Constants.java
+++ HoloDesign-ver.3/Sofbus24/src/bg/znestorov/sofbus24/utils/Constants.java
@@ -50,14 +50,14 @@ public class Constants {
 	/**
 	 * Metro Schedule
 	 */
-	public static final String METRO_STATION_URL = "https://sofia-stations.googlecode.com/svn/MetroSchedule/schedule/Station%s.xml";
+	public static final String METRO_STATION_URL = "https://sofia-stations.googlecode.com/svn/HoloDesign-ver.3/MetroSchedule/schedule/Station%s.xml";
 
 	/**
 	 * About
 	 */
-	public static final String CONFIGURATION_URL = "https://sofia-stations.googlecode.com/svn/Sofbus24_ConfigData/ConfigurationDetails/configuration.xml";
-	public static final String CONFIGURATION_STATIONS_DB_URL = "https://sofia-stations.googlecode.com/svn/Sofbus24_ConfigData/Databases/stations.db";
-	public static final String CONFIGURATION_VEHICLES_DB_URL = "https://sofia-stations.googlecode.com/svn/Sofbus24_ConfigData/Databases/vehicles.db";
+	public static final String CONFIGURATION_URL = "https://sofia-stations.googlecode.com/svn/HoloDesign-ver.3/ConfigData/ConfigurationDetails/configuration.xml";
+	public static final String CONFIGURATION_STATIONS_DB_URL = "https://sofia-stations.googlecode.com/svn/HoloDesign-ver.3/ConfigData/Databases/stations.db";
+	public static final String CONFIGURATION_VEHICLES_DB_URL = "https://sofia-stations.googlecode.com/svn/HoloDesign-ver.3/ConfigData/Databases/vehicles.db";
 	public static final String CONFIGURATION_PREF_NAME = "configuration";
 	public static final String CONFIGURATION_PREF_STATIONS_KEY = "CurrentStationsDBVersion";
 	public static final String CONFIGURATION_PREF_VEHICLES_KEY = "CurrentVehiclesDBVersion";
