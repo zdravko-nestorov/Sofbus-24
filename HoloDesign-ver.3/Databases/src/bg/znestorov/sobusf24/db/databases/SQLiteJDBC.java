e5066ac36a45ba146a96fa3630b5c9ef3ebbdc6d
zanio89 <zanio89@32671852-0f69-9b20-fc9e-79ad69c838e8>
2014-10-15 23:42:02 +0000

    Updating the DB files (versions 3 and 5), updating the metro stations, updating the DB code.

--- HoloDesign-ver.3/Databases/src/bg/znestorov/sobusf24/db/databases/SQLiteJDBC.java
+++ HoloDesign-ver.3/Databases/src/bg/znestorov/sobusf24/db/databases/SQLiteJDBC.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import bg.znestorov.sofbus24.db.entity.Station;
@@ -26,10 +28,8 @@ public class SQLiteJDBC {
 			logger.info("Opened vehicles database successfully");
 
 			stmt = c.createStatement();
-			/*
-			 * IMPORTANT: DO NOT DELETE THE TABLE CONTENT
-			 * stmt.executeUpdate("DELETE FROM vehicles;"); c.commit();
-			 */
+			stmt.executeUpdate("DELETE FROM vehicles;");
+			c.commit();
 
 			for (Vehicle vehicle : vehiclesList) {
 				String sql = "INSERT INTO vehicles (number, type, direction) "
@@ -52,45 +52,54 @@ public class SQLiteJDBC {
 			System.exit(0);
 		}
 
-		logger.info("Total vehicles = " + totalVehicles
-				+ ", Inserted vehicles = " + insertedVehicles
-				+ ", Not found vehicles = "
+		logger.info("Total vehicles (from SKGT) = " + totalVehicles
+				+ ", Inserted vehicles (in DB) = " + insertedVehicles
+				+ ", Not found vehicles (in DB) = "
 				+ (totalVehicles - insertedVehicles));
 	}
 
 	public static void createStationsDatabase(Logger logger,
 			ArrayList<Station> stationsList) {
+		String sql = null;
 		Connection c = null;
 		Statement stmt = null;
 
 		int totalStations = 0;
 		int insertedStations = 0;
 		int updatedStations = 0;
+		int deletedStations = 0;
+
+		Set<String> stationsNumbersList = new HashSet<String>();
 
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:database/stations.db");
 			c.setAutoCommit(false);
-			logger.info("Opened vehicles database successfully");
+			logger.info("Opened stations database successfully");
 
 			stmt = c.createStatement();
 			/*
 			 * IMPORTANT: DO NOT DELETE THE TABLE CONTENT
-			 * stmt.executeUpdate("DELETE FROM vehicles;"); c.commit();
+			 * stmt.executeUpdate("DELETE FROM stations;"); c.commit();
 			 */
 
+			// Insert or update the stations in the database
 			for (Station station : stationsList) {
-				String sql = "INSERT INTO stations (number, name, latitude, longitude, type) "
+				sql = "INSERT INTO stations (number, name, latitude, longitude, type) "
 						+ "VALUES ('%s', '%s', '%s', '%s', '%s');";
 				sql = String.format(sql, station.getNumber(),
 						station.getName(), station.getLatitude(),
 						station.getLongitude(), station.getType());
+
+				// Add the station number to the list
+				stationsNumbersList.add(station.getNumber());
+
 				try {
 					totalStations++;
 					stmt.executeUpdate(sql);
 					insertedStations++;
 				} catch (Exception e1) {
-					sql = "UPDATE stations set name = '%s' where number='%s';";
+					sql = "UPDATE stations SET name = '%s' WHERE number='%s';";
 					sql = String.format(sql, station.getName(),
 							station.getNumber());
 
@@ -104,6 +113,32 @@ public class SQLiteJDBC {
 				}
 			}
 
+			// Get all stations from the database and delete the not needed
+			try {
+				sql = "SELECT * FROM stations;";
+				stmt.executeQuery(sql);
+				// TODO: Get all stations
+				
+				for (Station station : stationsList) {
+					String stationNumber = station.getNumber();
+
+					if (!stationsNumbersList.contains(stationNumber)) {
+						sql = "DELETE FROM stations WHERE number='%s';";
+						sql = String.format(sql, stationNumber);
+
+						try {
+							stmt.executeUpdate(sql);
+							deletedStations++;
+						} catch (Exception e2) {
+							logger.info("Problem with deleting a station with number="
+									+ station.getNumber());
+						}
+					}
+				}
+			} catch (Exception e2) {
+				logger.info("Problem with retrieving all stations from the database.");
+			}
+
 			stmt.close();
 			c.commit();
 			c.close();
@@ -112,10 +147,11 @@ public class SQLiteJDBC {
 			System.exit(0);
 		}
 
-		logger.info("Total stations = " + totalStations
-				+ ", Inserted stations = " + insertedStations
-				+ ", Updated stations = " + updatedStations
-				+ ", Not found stations = "
+		logger.info("Total stations (from SKGT) = " + totalStations
+				+ ", Inserted stations (in DB) = " + insertedStations
+				+ ", Updated stations (in DB) = " + updatedStations
+				+ ", Deleted stations (from DB) = " + deletedStations
+				+ ", Not found stations (in DB) = "
 				+ (totalStations - insertedStations - updatedStations));
 	}
 }
