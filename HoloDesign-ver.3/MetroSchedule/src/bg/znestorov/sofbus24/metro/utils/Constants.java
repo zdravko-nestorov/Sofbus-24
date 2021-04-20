9883a57c35029d2c5a2d376092de634c4d8f80ae
zdravkonestorov <zdravko.nestorov@tide.co>
2021-04-20 11:16:49 +0300

    [20.04.2021] Updating Vehicles and Stations

--- HoloDesign-ver.3/MetroSchedule/src/bg/znestorov/sofbus24/metro/utils/Constants.java
+++ HoloDesign-ver.3/MetroSchedule/src/bg/znestorov/sofbus24/metro/utils/Constants.java
@@ -31,11 +31,11 @@ public class Constants {
 	 */
 	public static final String[] STATION_WEEKDAY_SCHEDULE_URL = {
 			"https://schedules.sofiatraffic.bg/server/html/schedule_load/8451/%s/%s",
-			"https://schedules.sofiatraffic.bg/server/html/schedule_load/9931/%s/%s"
+			"https://schedules.sofiatraffic.bg/server/html/schedule_load/10333/%s/%s"
 	};
 	public static final String[] STATION_HOLIDAY_SCHEDULE_URL = {
 			"https://schedules.sofiatraffic.bg/server/html/schedule_load/6621/%s/%s",
-			"https://schedules.sofiatraffic.bg/server/html/schedule_load/9932/%s/%s"
+			"https://schedules.sofiatraffic.bg/server/html/schedule_load/10336/%s/%s"
 	};
 
 	public static final String METRO_REGEX_TIME = ".*?<a href=\"#\"( class=\"incomplete_course)?[^^]*?onclick=\"Raz.exec[^^]*?'show_course',.*?return false;\">(.*?)</a>";
